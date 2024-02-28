05ed96c3f18dbc9d550866348e2e622a82c6e098
piero.carimi <pierocarimi@gmail.com>
2024-02-28 22:30:33 +0100

    feat(fix): fixed fetchProducts in index.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const fetchProducts = async () => {
     try {
-      const res = await fetch('http://localhost:3000/api/products');
+      const res = await fetch('api/products');
       const data: Product[] = await res.json();
       setProducts(data);
     }catch (error) {
