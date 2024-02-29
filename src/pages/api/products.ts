da14cac0ebf2e1fb73c6fa458180cac8c0f28108
piero.carimi <pierocarimi@gmail.com>
2024-02-29 09:53:50 +0100

    feat(fetch): updated api fetch url

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const response = await fetch('https://mockend.up.railway.app/api/products');
+    const response = await fetch('products-node-production.up.railway.app');
     const products = await response.json();
 
     res.status(200).json(products);
