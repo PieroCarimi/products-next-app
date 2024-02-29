f7338737c2ee5ea8b36df37de52efd556db8f4cf
piero.carimi <pierocarimi@gmail.com>
2024-02-29 11:38:25 +0100

    feat(fetch): updated url

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-fetch';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const response = await fetch('https://products-node-production.up.railway.app/api/products');
+    const response = await fetch('https://products-node-production.up.railway.app/');
     const products = await response.json();
 
     res.status(200).json(products);
